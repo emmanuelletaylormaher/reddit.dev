ba8405e0a40391bdbcc32c54a02d857b6f4f355f
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-31 01:16:16 -0500

    finished up navbar

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -2,17 +2,20 @@
 	<a class="navbar-brand" href="/">fake reddit</a>
 
 	<ul class="nav navbar-nav navbar-right">
-		<li><a class="navbar-brand" href="/posts">posts</a></li>
+		<li><a class="navbar-brand" href="{{action('PostsController@index')}}">posts</a></li>
 	</ul>
 
-	<ul class="nav navbar-nav navbar-right">
-		<li><a class="navbar-brand" href="{{action('Auth\AuthController@getRegister')}}">register</a></li>
-		<li><a class="navbar-brand" href="{{action('PostsController@create')}}">login</a></li>
-	</ul>
+	@if(Auth::check())
 	<ul class="nav navbar-nav navbar-right">
 		<li><a class="navbar-brand" href="{{action('PostsController@create')}}">create post</a></li>
 		<li><a class="navbar-brand" href="{{action('Auth\AuthController@getLogout')}}">logout</a></li>	
 	</ul>
+	@else
+	<ul class="nav navbar-nav navbar-right">
+		<li><a class="navbar-brand" href="{{action('Auth\AuthController@getRegister')}}">register</a></li>
+		<li><a class="navbar-brand" href="{{action('Auth\AuthController@getLogin')}}">login</a></li>
+	</ul>
+	@endif
 
 	<form class= "navbar-form form-inline">
 		<input class="form-control mr-sm-2" type="text" placeholder="search">
